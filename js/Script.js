bf01347f2c00fba64d9ce49a5350e799495b3b70
tdepamphilis <62816494+tdepamphilis@users.noreply.github.com>
2020-05-03 17:06:05 -0300

    First Commit
    new:
    - css & js files
    - index head
    - index body
    - placeholder body image
    - Alert on required input fields

---
+++ js/Script.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+function enviar(){
+
+    var textmail = document.getElementById("mail").value;
+    var textname = document.getElementById("name").value;
+    var textmessage = document.getElementById("message").value;
+    if (textmail == "" || textname == "" || textmessage == "")
+    {
+        alert("Complete los campos");
+    }
+}
